9a6e0c08eec8f02019d4627c8b7a6e20777ae4bf
Miloš Milanović <mmilanovic016@gmail.com>
2021-09-27 17:24:34 +0200

    Update analytics tag

--- src/client/bootstrap/analytics.js
+++
@@ -1,6 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-const gtag = () => {
-	dataLayer?.push(arguments);
-};
-gtag('js', new Date());
-gtag('config', 'UA-166027666-1');
--- src/client/bootstrap/index.js
+++ src/client/bootstrap/index.js
@@ -1,2 +1,1 @@
-import './analytics';
 import './registerWorker';
