b47275f507dec88d3968c06b8ac8a62cfd275153
Miloš Milanović <mmilanovic016@gmail.com>
2022-11-13 22:44:32 +0100

    Reorganize code

---
+++ src/components/Block.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { IconBriefcase, IconCalendar, IconLocation } from '../icons';
+
+import css from './Block.module.css';
+
+const Block = ({ item, type = 'work' }) => {
+	return (
+		<li className={css?.block}>
+			<img
+				alt={item?.name}
+				className={css?.blockLogo}
+				src={`/static/${type}/${item?.img}.png`}
+			/>
+			<div className={css?.blockContent}>
+				<span className={css?.blockContentTitle}>{item?.title}</span>
+				<div className={css?.blockContentDescription}>
+					<div className={css?.blockContentLeftSide}>
+						<span className={css?.blockContentItem}>
+							<IconBriefcase />
+							<span>{item?.name}</span>
+						</span>
+						<span className={css?.blockContentItem}>
+							<IconLocation />
+							<span>{item?.location}</span>
+						</span>
+					</div>
+					<div className={css?.blockContentRightSide}>
+						<span className={css?.blockContentItem}>
+							<IconCalendar />
+							<span>
+								{item?.from} - {item?.to || 'Present'}
+							</span>
+						</span>
+					</div>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+export { Block };
---
+++ src/data/index.js
@@ -0,0 +1,61 @@
+export const information = [
+	{ key: 'Age', value: new Date()?.getFullYear() - 1993 },
+	{ key: 'Location', value: 'Belgrade' },
+	{ key: 'Time zone', value: 'GMT+1' }
+];
+
+export const skills = ['JavaScript', 'React', 'HTML', 'CSS', 'SQL', 'Git'];
+
+export const work = [
+	{
+		from: 'January 2021',
+		to: null,
+		name: 'LoanQ',
+		title: 'Full-Stack Web Developer',
+		img: 'lnq',
+		location: 'Sydney, Australia'
+	},
+	{
+		from: 'September 2018',
+		to: null,
+		name: 'Salestrekker',
+		title: 'Full-Stack Web Developer',
+		img: 'st',
+		location: 'Sydney, Australia'
+	},
+	{
+		from: 'December 2016',
+		to: 'July 2018',
+		name: 'High-Tech Bridge',
+		title: 'Junior Security Engineer',
+		img: 'htb',
+		location: 'Geneva, Switzerland'
+	}
+];
+
+export const education = [
+	{
+		from: 2016,
+		to: 2018,
+		name: 'Singidunum University',
+		title: 'Master of Informatics',
+		location: 'Belgrade, Serbia',
+		img: 'singidunum'
+	},
+	{
+		from: 2012,
+		to: 2016,
+		name: 'Singidunum University',
+		title: 'Bachelor of Science in Informatics',
+		location: 'Belgrade, Serbia',
+		img: 'singidunum'
+	},
+	{
+		from: 2008,
+		to: 2012,
+		name: 'ETHS "Nikola Tesla" in Belgrade',
+		title: 'Computer Technician',
+		location: 'Belgrade, Serbia',
+		img: 'tesla'
+	}
+];
