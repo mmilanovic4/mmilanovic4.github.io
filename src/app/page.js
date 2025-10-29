4110aee2f4e1b3021e64543ff1e353db148c9a54
Miloš Milanović <mmilanovic016@gmail.com>
2025-10-29 17:54:09 +0100

    chore: add files

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,47 @@
 import Image from "next/image";
+import {
+  SiGithub,
+  SiGmail,
+  SiInstagram,
+  SiYoutube,
+} from "@icons-pack/react-simple-icons";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen flex flex-col">
+      <header className="flex flex-col items-center justify-center px-2">
+        <div className="mt-2 mb-4">
+          <Image
+            alt="Profile"
+            className="rounded"
+            loading="eager"
+            src="/profile.jpg"
+            width={500}
+            height={375}
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+        <h1 className="mb-2 text-center">Miloš Milanović</h1>
+        <p className="font-light text-center">
+          Full-stack web developer based in Belgrade, Serbia.
+        </p>
+        <div className="flex gap-4 my-4">
+          <a href="mailto:mmilanovic016@gmail.com">
+            <SiGmail className="text-[#EA4335]" />
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+          <a href="https://github.com/mmilanovic4" target="_blank">
+            <SiGithub className="text-[#181717]" />
+          </a>
+          <a href="https://www.instagram.com/mmilanovic4/" target="_blank">
+            <SiInstagram className="text-[#FF0069]" />
+          </a>
+          <a href="https://www.youtube.com/@mmilanovic4" target="_blank">
+            <SiYoutube className="text-[#FF0000]" />
           </a>
         </div>
-      </main>
+      </header>
+      <footer className="mt-auto p-4 text-sm text-center">
+        &copy; Miloš Milanović {new Date().getFullYear()}.
+      </footer>
     </div>
   );
 }
