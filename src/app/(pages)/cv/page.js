ee9c01cb125d99e5ab4a58793796cea1d9674f1a
Miloš Milanović <mmilanovic016@gmail.com>
2025-10-30 16:32:24 +0100

    chore: change port and switch on ssl

--- src/app/(pages)/cv/page.js
+++ src/app/(pages)/cv/page.js
@@ -6,22 +6,19 @@ export default function Resume() {
         <ul className="flex flex-col gap-2">
           <li>
             <span className="w-32 inline-block">2020 - now</span>
-            <span>Full-stack web developer</span>
-            <span className="ml-32 block">
+            <span>
               <strong>LoanQ</strong>, Sydney, Australia
             </span>
           </li>
           <li>
             <span className="w-32 inline-block">2018 - now</span>
-            <span>Full-stack web developer</span>
-            <span className="ml-32 block">
+            <span>
               <strong>Salestrekker</strong>, Sydney, Australia
             </span>
           </li>
           <li>
             <span className="w-32 inline-block">2016 - 2018</span>
-            <span>Junior Security Engineer</span>
-            <span className="ml-32 block">
+            <span>
               <strong>High-Tech Bridge</strong>, Geneva, Switzerland
             </span>
           </li>
