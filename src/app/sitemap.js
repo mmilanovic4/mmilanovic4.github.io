d9814d78828f4ed26afcf349a645ab49d3e876a2
Miloš Milanović <mmilanovic016@gmail.com>
2026-05-02 18:28:54 +0200

    chore: add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  const posts = getAllPosts();
+
+  const blogUrls = posts.map((post) => ({
+    url: `https://mmilanovic4.github.io/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+  }));
+
+  return [
+    {
+      url: "https://mmilanovic4.github.io",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mmilanovic4.github.io/resume",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mmilanovic4.github.io/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mmilanovic4.github.io/blog",
+      lastModified: new Date(),
+    },
+    ...blogUrls,
+  ];
+}
