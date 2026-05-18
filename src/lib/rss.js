7c63c64304a8d3c2ffec93eba77a37dd632d4b45
Miloš Milanović <mmilanovic016@gmail.com>
2026-05-18 12:58:18 +0200

    fix: remove redundant fn call

--- src/lib/rss.js
+++ src/lib/rss.js
@@ -2,36 +2,31 @@ import fs from "fs";
 import path from "path";
 import { getAllPosts } from "./blog.js";
 
+const SITE_URL = "https://mmilanovic4.github.io";
+
 export function generateRSS() {
   const posts = getAllPosts();
-
-  const siteUrl = "https://mmilanovic4.github.io";
 
   const rss = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
   <channel>
     <title>Miloš Milanović</title>
-    <link>${siteUrl}</link>
+    <link>${SITE_URL}</link>
     <description>Latest blog posts</description>
-
     ${posts
       .map(
         (post) => `
-      <item>
-        <title><![CDATA[${post.title}]]></title>
-        <link>${siteUrl}/blog/${post.slug}</link>
-        <guid>${siteUrl}/blog/${post.slug}</guid>
-        <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-        <description><![CDATA[${post.description || ""}]]></description>
-      </item>
-    `,
+    <item>
+      <title><![CDATA[${post.title}]]></title>
+      <link>${SITE_URL}/blog/${post.slug}</link>
+      <guid>${SITE_URL}/blog/${post.slug}</guid>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      <description><![CDATA[${post.description || ""}]]></description>
+    </item>`,
       )
       .join("")}
-
   </channel>
 </rss>`;
 
   fs.writeFileSync(path.join(process.cwd(), "public/rss.xml"), rss);
 }
-
-generateRSS();
