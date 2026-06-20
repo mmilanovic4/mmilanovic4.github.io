7c69d5c4c5d05b9ac227a28b1ba235347f297c68
Miloš Milanović <mmilanovic016@gmail.com>
2026-06-20 22:33:35 +0200

    fix: remove trailing slash on void elements

---
+++ scripts/strip-void-slashes.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+
+const VOID =
+  "area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr";
+const re = new RegExp(`<(${VOID})([^>]*?)\\s*/>`, "gi");
+
+function fix(dir) {
+  for (const entry of fs.readdirSync(dir, { withFileTypes: true })) {
+    const full = path.join(dir, entry.name);
+    if (entry.isDirectory()) fix(full);
+    else if (entry.name.endsWith(".html")) {
+      const html = fs.readFileSync(full, "utf8");
+      fs.writeFileSync(full, html.replace(re, "<$1$2>"));
+    }
+  }
+}
+
+fix("out");
