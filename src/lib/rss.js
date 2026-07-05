a2aab7195ec2cc05be42d8c44898820a1bb6d96c
Miloš Milanović <mmilanovic016@gmail.com>
2026-07-05 21:26:46 +0200

    fix: rss generator

--- src/lib/rss.js
+++ src/lib/rss.js
@@ -1,31 +1,34 @@
 import fs from "fs";
 import path from "path";
+import { Feed } from "feed";
 import { getAllPosts } from "./blog.js";
 import { AUTHOR, BASE_URL } from "./metadata.js";
 
 export function generateRSS() {
   const posts = getAllPosts();
 
-  const rss = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-  <channel>
-    <title>${AUTHOR}</title>
-    <link>${BASE_URL}</link>
-    <description>Latest blog posts</description>
-    ${posts
-      .map(
-        (post) => `
-    <item>
-      <title><![CDATA[${post.title}]]></title>
-      <link>${BASE_URL}/blog/${post.slug}</link>
-      <guid>${BASE_URL}/blog/${post.slug}</guid>
-      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-      <description><![CDATA[${post.description || ""}]]></description>
-    </item>`,
-      )
-      .join("")}
-  </channel>
-</rss>`.trim();
+  const feed = new Feed({
+    title: AUTHOR,
+    description: "Latest blog posts",
+    id: BASE_URL,
+    link: BASE_URL,
+    language: "en",
+    copyright: `© ${AUTHOR}, ${new Date().getFullYear()}.`,
+    author: { name: AUTHOR, link: BASE_URL },
+    feedLinks: { rss: `${BASE_URL}/rss.xml` },
+    generator: false,
+  });
 
-  fs.writeFileSync(path.join(process.cwd(), "public/rss.xml"), rss);
+  for (const post of posts) {
+    const url = `${BASE_URL}/blog/${post.slug}`;
+    feed.addItem({
+      title: post.title,
+      id: url,
+      link: url,
+      description: post.description || "",
+      date: new Date(post.date),
+    });
+  }
+
+  fs.writeFileSync(path.join(process.cwd(), "public/rss.xml"), feed.rss2());
 }
