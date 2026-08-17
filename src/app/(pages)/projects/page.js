da0a8c00f5d603ad61811f87071c6a6fc8e88531
Miloš Milanović <mmilanovic016@gmail.com>
2026-08-17 22:42:13 +0200

    fix: console warn

--- src/app/(pages)/projects/page.js
+++ src/app/(pages)/projects/page.js
@@ -54,6 +54,7 @@ export default function Projects() {
                     alt={project.name}
                     width={16}
                     height={16}
+                    className="h-4 w-4"
                   />
                 )}
                 {project.name}
